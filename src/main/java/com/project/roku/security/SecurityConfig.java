bd7d457a77e989de9be57e7f4a36f52d9a1a286b
conallmcgrath <conallm742@gmail.com>
2023-10-12 14:30:48 +0100

    mike and susan can now log in. So we have our connection to the sql database, and users can log in with their credentials, and information is displayed depending on their role.

--- src/main/java/com/project/roku/security/SecurityConfig.java
+++ src/main/java/com/project/roku/security/SecurityConfig.java
@@ -28,9 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
-                        .requestMatchers("/").hasRole("NURSE")
-                        .requestMatchers("/leaders/**").hasRole("DOCTOR") // the /** means all subdirectories
-                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .requestMatchers("/").hasAnyRole("NURSE","DOCTOR","ADMIN")
                         .anyRequest().authenticated()
         )
                 .formLogin(form ->
@@ -46,29 +44,4 @@ public class SecurityConfig {
                 );
         return http.build();
     }
-        /*
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
-    }
-
-     */
-
 }
