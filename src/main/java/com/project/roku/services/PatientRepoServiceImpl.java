849d4895c73a763dc36fbd480f3f8b38087cf616
conallmcgrath <conallm742@gmail.com>
2023-10-20 16:40:13 +0100

    so at this stage we can find the table, but the table is empty

--- src/main/java/com/project/roku/services/PatientRepoServiceImpl.java
+++ src/main/java/com/project/roku/services/PatientRepoServiceImpl.java
@@ -4,10 +4,12 @@ import com.project.roku.dao.PatientRepository;
 import com.project.roku.entity.Employee;
 import com.project.roku.entity.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PatientRepoServiceImpl implements PatientRepoService {
 
 
