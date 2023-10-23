0b7c11f4f19b241e6aff67b1095605f1281cbee6
conallmcgrath <conallm742@gmail.com>
2023-10-23 16:57:37 +0100

    so I added the form for the prescription management but there is an issue with the id, it's getting an error.

--- src/main/java/com/project/roku/controller/PrescriptionController.java
+++ src/main/java/com/project/roku/controller/PrescriptionController.java
@@ -3,15 +3,22 @@ package com.project.roku.controller;
 import com.project.roku.entity.Patient;
 import com.project.roku.services.PatientRepoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/prescriptions")
 public class PrescriptionController {
+
+    // injection countries for medication list
+    @Value("${meds}")
+    private List<String> meds;
 
     private PatientRepoService patientService;
 
@@ -21,27 +28,17 @@ public class PrescriptionController {
     }
 
     @GetMapping("/showPresForm")
-    public String showPresForm(@RequestParam("patientId") int theId Model theModel){
+    public String showPresForm(@RequestParam("patientId") int theId, Model theModel){
         // create model attribute to bind the data from to
-        Patient thePatient = patientS
-
-        theModel.addAttribute("patient", thePatient);
-        return "prescriptions/prescription-form";
-    }
-
-
-    /*
-       @GetMapping("/showFormForUpdate")
-    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
-        // get the employee from the service
-        Employee theEmployee = employeeService.findById(theId);
+        Patient thePatient = patientService.findById(theId);
 
         // set the employee in the model to prepopulate the model/form
-        theModel.addAttribute("employee", theEmployee);
+        theModel.addAttribute("patient", thePatient);
+
+        // adding the drop down meds to the model
+        theModel.addAttribute("medications", meds);
+
         // send over to our form
-
-        return "employees/employee-form";
+        return "prescriptions/prescription-form";
     }
-     */
-
 }
